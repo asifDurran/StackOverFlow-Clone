2e8b4362d18488de0f9f7490305fdee51416e75f
unknown <ma223xr@lnu.se>
2019-10-24 12:25:20 +0200

    I generated Auth and neccessary installation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Stack Over Flow Cloning
                 </div>
 
                 <div class="links">
