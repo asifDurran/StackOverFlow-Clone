f5e64c2ade43c27360527fce4d2ea5478fc9f960
unknown <ma223xr@lnu.se>
2019-11-01 20:39:32 +0100

    Generated fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    
-
-
     public function question()
     {
         return $this->belongsTo(Question::class);
@@ -17,5 +14,14 @@ class Answer extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+        });
     }
 }
